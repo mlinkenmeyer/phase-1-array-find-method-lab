aef56f9344d2b06f9a8eb5e4fbc4612f2f64fc0e
Madison <mlinkenmeyer@gmail.com>
2023-08-11 19:17:46 -0700

    array find lab complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+const record = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+  //...
+];
+
+function superbowlWin(team) {
+  const yearOfTheWin = team.find((entry) => entry.result === "W");
+  return yearOfTheWin ? yearOfTheWin.year : undefined;
+}
+
+console.log(superbowlWin(record)); // Call the function and log the result
+
+const winningRecord = record.find((entry) => entry.result === "W");
+console.log(winningRecord);
